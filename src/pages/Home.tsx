02dae6478c6c7efc243fd095cab935364fe5e272
Rodrigo Ferreira Schaer <rodrigo.fscs@gmail.com>
2021-06-26 14:33:51 -0300

    versao da aula 5

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -51,6 +51,10 @@ export function Home() {
             return;
         }
 
+        if (roomRef.val().endedAt){
+            alert('Room already closed')
+        }
+
         history.push(`/rooms/${roomCode}`)
     }
 
